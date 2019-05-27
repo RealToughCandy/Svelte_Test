e393bbe77d20e1410b7a4036e4449ea813ba5ef1
RealToughCandy <barncatdigital@gmail.com>
2019-05-27 03:43:19 -0500

    Svelte Test

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import NewProduct from "./NewProduct.svelte";
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: deepskyblue;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+
+
+<section>
+<div>
+<label for="title">Title</label>
+<input type="text" id="title">
+</div>
+
+<div>
+<label for="price">Price</label>
+<input type="number" id="price">
+</div>
+
+<div>
+<label for="description">Description</label>
+<textarea rows="3" id="description"></textarea>
+</div>
+
+</section>
+
+<NewProduct />
---
+++ src/NewProduct.svelte
@@ -0,0 +1,46 @@
+<style>
+    div {
+        margin: 1rem;
+        box-shadow: 0 2px 8px rgba(0,0,0,26);
+        padding: 1rem;
+        border-radius: 5px;
+        background: white;
+    }
+
+    h1 {
+        font-size: 1.25rem;
+        margin: 0.25rem 0;
+
+     }
+    h2 {
+        font-size: 1rem;
+        margin: 0.25rem 0;
+        color: darkgray;
+    }
+
+    p {
+        margin: 0.25rem 0;
+        }
+    button {
+        font: inherit;
+        padding: 0.15rem 0.5rem;
+        background: rgb(162, 126, 162);
+        border: 1px solid black;
+        color: white;
+        cursor: pointer;
+        }
+
+        button:hover, 
+        button:active {
+            box-shadow: 1px 1px 6px rgba(0,0,0, 26);
+        }
+</style>
+
+
+
+<div>
+    <h1>Title</h1>
+    <h2>Price</h2>
+    <p>Description</p>
+    <button>Add to Cart</button>
+</div>
